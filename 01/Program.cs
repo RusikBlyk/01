8277e842561612e011a6b65f6125eb036ee0bfe2
RusikBlyk <rusikblyk@gmail.com>
2016-11-06 12:58:47 +0200

    urok15
    Hello !

---
+++ 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3 {
+	class Program{
+
+
+		static void Main (string[] args) {
+			int speed = 1;
+			int distance =20;
+			int time = 0;
+			while (distance>=0)//16;11;5;
+			{
+				distance -= speed;//16-5=11;11-6=5;5-7=-2;
+				speed++;//6;7;8;
+				time++;//1;2;3;
+
+			}
+			Console.WriteLine(time);
+			Console.Read ();
+		}
+
+	}
+}
+
